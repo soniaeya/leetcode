d70a0749622ee6c181bbd02d2e2ca10a6a4f4bfa
Sonia Li <sonialxy00@gmail.com>
2023-04-27 17:38:04 -0400

    initial commit

---
+++ top-interview-questions/src/ReverseString.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void reverseString(char[] s) {
+
+        for (int i = 0; i< (s.length/2);i++){
+            char temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+        }
+        System.gc();
+
+        System.out.println(Arrays.toString(s));
+    }
+    public static void main(String[] args) {
+        char [] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+
+    }
+
+}
